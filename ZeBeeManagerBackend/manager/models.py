919eda7d2f10cf3504b3b317e5a1b16aa342ca44
O-A-Mi <luiz.alves.frr@gmail.com>
2025-07-10 09:24:30 -0300

    Fix: Unique seller_email

--- ZeBeeManagerBackend/manager/models.py
+++ ZeBeeManagerBackend/manager/models.py
@@ -36,7 +36,7 @@ class Client(models.Model):
     seller_name = models.CharField(max_length=200, verbose_name="Nome do Cliente")
     store_name = models.CharField(max_length=200, unique=True, verbose_name="Nome da Loja")
     seller_id = models.CharField(max_length=50, verbose_name="ID do Cliente", blank=True, null=True)
-    seller_email = models.EmailField(unique=True, verbose_name="Email do Cliente", blank=True, null=True)
+    seller_email = models.EmailField(verbose_name="Email do Cliente", blank=True, null=True)
     status = models.CharField(max_length=20, choices=[('Ativo', 'Ativo'), ('Inativo', 'Inativo')], default='Ativo')
     contracted_plan = models.CharField(max_length=100, verbose_name="Plano Contratado")
     plan_value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor do Plano")
