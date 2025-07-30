57dea33d4ee28a3700f9ea918bdee8803253ccd2
ZeBee-AIR <zebee.air@gmail.com>
2025-07-30 11:23:30 -0300

    Fix: aviso

--- ZeBeeManagerFrontend/src/components/MainLayout.tsx
+++ ZeBeeManagerFrontend/src/components/MainLayout.tsx
@@ -5,9 +5,9 @@ import Footer from './Footer';
 const MainLayout = () => {
     return (
         <div className="flex flex-col min-h-screen bg-background">
+            <div className="w-100 bg-red-600 text-yellow-50 text-center text-2xl">Você está no ambiente de testes.</div>
             <Navigation />
                 <main className="flex-grow">
-                    <div className="w-100 bg-red-600 text-yellow-50">Você está no ambiente de testes.</div>
                     <Outlet />
                 </main>
             <Footer />
