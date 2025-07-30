429375fb292f9266fb03d8e791849a6eeb248a96
ZeBee-AIR <zebee.air@gmail.com>
2025-07-30 11:21:16 -0300

    Fix: aviso para ambiente de testes

--- ZeBeeManagerFrontend/src/components/MainLayout.tsx
+++ ZeBeeManagerFrontend/src/components/MainLayout.tsx
@@ -7,6 +7,7 @@ const MainLayout = () => {
         <div className="flex flex-col min-h-screen bg-background">
             <Navigation />
                 <main className="flex-grow">
+                    <div className="w-100 bg-red-600 text-yellow-50">Você está no ambiente de testes.</div>
                     <Outlet />
                 </main>
             <Footer />
