892ea820411db083eb8fe7b037ef06980376219f
ZeBee-AIR <zebee.air@gmail.com>
2025-07-30 15:24:14 -0300

    Fix: Ajuste no tempo do token após testes

--- ZeBeeManagerBackend/ZeBeeManagerBackend/settings.py
+++ ZeBeeManagerBackend/ZeBeeManagerBackend/settings.py
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12), # Token expira em 12 horas
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
@@ -149,8 +149,8 @@ SIMPLE_JWT = {
     "JTI_CLAIM": "jti",
 
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=1),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(minutes=1),
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 
     "TOKEN_OBTAIN_SERIALIZER": "dj_rest_auth.jwt_auth.get_refresh_view().serializer_class",
     "TOKEN_REFRESH_SERIALIZER": "dj_rest_auth.jwt_auth.get_refresh_view().serializer_class",
